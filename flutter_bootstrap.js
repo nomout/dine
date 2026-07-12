ae42237112b4d373fa70c7bf8ee38cd6a1478b68
Dhairya <joshidhairaya2002@gmail.com>
2026-07-12 23:31:57 +0530

    deploy: rebuild diner web

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "902361290"
+    serviceWorkerVersion: "2031806677"
   }
 });
